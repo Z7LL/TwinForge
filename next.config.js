3b7c07fcd68780437429aa277ad28973db4514c0
Z7LL <108791502+Z7LL@users.noreply.github.com>
2026-08-06 12:20:47 +0400

    Fix build errors: Remove logo.css import, add webpack ignoreWarnings in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  webpack: (config) => {
+    config.ignoreWarnings = [
+      { module: /node_modules\/node-fetch\/lib\/index\.js/ },
+      { file: /node_modules\/node-fetch\/lib\/index\.js/ },
+    ];
+    return config;
   },
-  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
